a8a3f33c548f9333edc980d9a74399fc4da8beb9
Dan Kempton <kemptondan@gmail.com>
2020-07-29 21:22:20 -0400

    Adding comment to count function

--- vowels-in-a-string/app.js
+++ vowels-in-a-string/app.js
@@ -1,5 +1,6 @@
 const getCount = (str) => {
 	const vowels = 'a, e, i, o, u';
+	// filter the letters in the string, and check if each letter is a vowel
 	const count = Array.from(str).filter((letter) => vowels.includes(letter)).length;
 	return count;
 };
