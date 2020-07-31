3f2d0c94ec0a9eba35d20b55752333529102b67e
Dan Kempton <kemptondan@gmail.com>
2020-07-31 12:57:55 -0400

    Adding comments

--- fix-string-case/app.js
+++ fix-string-case/app.js
@@ -1,1 +1,3 @@
+// using replace method and a regex to check if the length of uppercase letters is greater than half the length of the total string
+// if true, return all string characters to upperCase, else return to lowerCase
 const solve = (str) => (str.replace(/[a-z]/g, '').length > str.length / 2 ? str.toUpperCase() : str.toLowerCase());
