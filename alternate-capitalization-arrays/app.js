34cec2378c18a36b3a436d847161c2c433ccecbe
Dan Kempton <kemptondan@gmail.com>
2020-08-13 12:58:26 -0400

    Adding odd variable

--- alternate-capitalization-arrays/app.js
+++ alternate-capitalization-arrays/app.js
@@ -3,4 +3,10 @@ function capitalize(str) {
     .split('')
     .map((letter, i) => (letter[i] % 2 === 0 ? letter.toUpperCase() : letter))
     .join('');
+  console.log(even);
+  const odd = str
+    .split('')
+    .map((letter, i) => (letter[i] !== 0 ? letter.toUpperCase() : letter))
+    .join('');
+  console.log(odd);
 }
