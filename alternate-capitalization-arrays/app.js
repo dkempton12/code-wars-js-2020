9025e70f25ea75db50f62b3ff4d50f25772a4f9b
Dan Kempton <kemptondan@gmail.com>
2020-08-13 13:00:15 -0400

    Updating odd variable if statement and returning array

--- alternate-capitalization-arrays/app.js
+++ alternate-capitalization-arrays/app.js
@@ -4,9 +4,12 @@ function capitalize(str) {
     .map((letter, i) => (letter[i] % 2 === 0 ? letter.toUpperCase() : letter))
     .join('');
   console.log(even);
+
   const odd = str
     .split('')
-    .map((letter, i) => (letter[i] !== 0 ? letter.toUpperCase() : letter))
+    .map((letter, i) => (letter[i] % 2 !== 0 ? letter.toUpperCase() : letter))
     .join('');
   console.log(odd);
+
+  return [even, odd];
 }
