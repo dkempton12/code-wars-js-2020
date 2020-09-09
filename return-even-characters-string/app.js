122e3db9f44256f3fcf13f25e3c6bbf972b29f12
Dan Kempton <kemptondan@gmail.com>
2020-09-09 18:38:44 -0400

    Testing - success

--- return-even-characters-string/app.js
+++ return-even-characters-string/app.js
@@ -2,3 +2,7 @@ const returnEvenChars = (string) =>
   string.length < 2 || string.length > 100
     ? 'invalid string'
     : string.split('').filter((x, i) => i % 2);
+
+const random = 'hellohowareyou';
+
+returnEvenChars(random);
